6db838d9995feae28ed46eba0e69696ce02ee0d9
HP <prabhubansode1982@gmail.com>
2024-11-03 02:45:08 +0530

    First Kafka Example

---
+++ src/main/java/com/pvp/ConsumerExample.java
@@ -0,0 +1,53 @@
+package com.pvp;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.common.config.SaslConfigs.SASL_JAAS_CONFIG;
+import static org.apache.kafka.common.config.SaslConfigs.SASL_MECHANISM;
+
+public class ConsumerExample {
+
+    public static void main(final String[] args) {
+
+        final Properties props = new Properties() {{
+            // User-specific properties that you must set
+            put(BOOTSTRAP_SERVERS_CONFIG, "pkc-921jm.us-east-2.aws.confluent.cloud:9092");
+            put(SASL_JAAS_CONFIG,         "org.apache.kafka.common.security.plain.PlainLoginModule required username='USLOWVBQGBMZZ6WX' password='R6idX4XX3WyGAHrrA5yJ46K5tdwruERt2mCMkYJieHdOcQxmfjVd9iCl7rrQ/ncB';");
+
+            // Fixed properties
+            put(KEY_DESERIALIZER_CLASS_CONFIG,   StringDeserializer.class.getCanonicalName());
+            put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+            put(GROUP_ID_CONFIG,                 "kafka-java-getting-started");
+            put(AUTO_OFFSET_RESET_CONFIG,        "earliest");
+            put(SECURITY_PROTOCOL_CONFIG,        "SASL_SSL");
+            put(SASL_MECHANISM,                  "PLAIN");
+        }};
+
+        final String topic = "notifications_topic";
+
+        try (final Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Arrays.asList(topic));
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    String key = record.key();
+                    String value = record.value();
+                    System.out.println(
+                            String.format("Consumed event from topic %s: key = %-10s value = %s", topic, key, value));
+                }
+            }
+        }
+    }
+
+}
