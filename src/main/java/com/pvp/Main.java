b52223eaa47b4f380b6b79864d77303d86af67ec
HP <prabhubansode1982@gmail.com>
2024-12-17 02:12:55 +0530

    First Kafka Example

--- src/main/java/com/pvp/Main.java
+++ src/main/java/com/pvp/Main.java
@@ -2,6 +2,6 @@ package com.pvp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Kafka!");
+        System.out.println("Hello Kafka! 123");
     }
 }
